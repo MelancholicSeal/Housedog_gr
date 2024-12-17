5155bc6580c4faad1d0a0217c5cca9c694d36c19
it2022115 <it2022115@hua.gr>
2024-12-17 16:22:58 +0200

    User class updated

--- src/main/java/gr/hua/dit/ds/ds_lab_2024/entities/User.java
+++ src/main/java/gr/hua/dit/ds/ds_lab_2024/entities/User.java
@@ -33,6 +33,26 @@ public class User {
     @Size(max = 120)
     private String password;
 
+    @NotBlank
+    @Size(max = 30)
+    private String firstName;
+
+    @NotBlank
+    @Size(max = 30)
+    private String lastName;
+
+    @NotBlank
+    @Size(max = 10)
+    private String phoneNumber;
+
+    @NotBlank
+    @Size(max = 9)
+    private String AFM;
+
+    @NotBlank
+    @Size(max = 8)
+    private String idNumber;
+
     @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(	name = "user_roles",
             joinColumns = @JoinColumn(name = "user_id"),
@@ -42,10 +62,15 @@ public class User {
     public User() {
     }
 
-    public User(String username, String email, String password) {
+    public User(String username, String email, String password, String firstName, String lastName, String phoneNumber, String AFM, String idNumber) {
         this.username = username;
         this.email = email;
         this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.AFM = AFM;
+        this.idNumber = idNumber;
     }
 
     public Integer getId() {
@@ -78,6 +103,46 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getFirstName(String firstName) {
+        return firstName;
+    }
+    
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName(String lastName) {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber(String phoneNumber) {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAFM(String AFM){
+        return AFM;
+    }
+    
+    public void setAFM(String AFM) {
+        this.AFM = AFM;
+    }
+
+    public String getIDNumber(String idNumber) {
+        return idNumber;
+    }
+
+    public void setIdNumber(String idNumber) {
+        this.idNumber = idNumber;
     }
 
     public Set<Role> getRoles() {
