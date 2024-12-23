db82bf4346f99c3ad3fb6506f378c159beecdd3a
it2022130 <it2022113@hua.gr>
2024-12-23 17:13:09 +0200

    Property Update

--- src/main/java/gr/hua/dit/ds/ds_lab_2024/entities/Property.java
+++ src/main/java/gr/hua/dit/ds/ds_lab_2024/entities/Property.java
@@ -53,6 +53,14 @@ public class Property {
         return address;
     }
 
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
     public String getCity() {
         return city;
     }
