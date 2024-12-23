e342e0d60d55a5fecfc47b78c9f01d5e18e2a76e
it2022130 <it2022113@hua.gr>
2024-12-23 17:24:24 +0200

    RentService

---
+++ src/main/java/gr/hua/dit/ds/ds_lab_2024/service/RentService.java
@@ -0,0 +1,35 @@
+package gr.hua.dit.ds.ds_lab_2024.service;
+
+import gr.hua.dit.ds.ds_lab_2024.entities.Rent;
+import gr.hua.dit.ds.ds_lab_2024.repositories.RentRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RentService {
+
+    private RentRepository rentRepository;
+
+    public RentService(RentRepository rentRepository) {
+        this.rentRepository = rentRepository;
+    }
+
+    @Transactional
+    public List<Rent> getRent() {
+        return rentRepository.findAll();
+    }
+
+    @Transactional
+    public void saveStudent(Rent rent) {
+        rentRepository.save(rent);
+    }
+
+    @Transactional
+    public Rent getRent(Integer rentId) {
+        return rentRepository.findById(rentId).get();
+    }
+
+}
