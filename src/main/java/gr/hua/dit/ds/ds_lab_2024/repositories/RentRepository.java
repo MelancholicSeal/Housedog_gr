518531031cf1a48b27d771ae4a857c489b3b124b
it2022130 <it2022113@hua.gr>
2024-12-23 16:32:35 +0200

    Updated Repositories

--- src/main/java/gr/hua/dit/ds/ds_lab_2024/repositories/RentRepository.java
+++ src/main/java/gr/hua/dit/ds/ds_lab_2024/repositories/RentRepository.java
@@ -1,9 +1,9 @@
 package gr.hua.dit.ds.ds_lab_2024.repositories;
 
-import gr.hua.dit.ds.ds_lab_2024.entities.Course;
+import gr.hua.dit.ds.ds_lab_2024.entities.Rent;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CourseRepository extends JpaRepository<Course, Integer> {
+public interface RentRepository extends JpaRepository<Rent, Integer> {
 }
