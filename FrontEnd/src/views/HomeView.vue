8bb9413985d06fe6860d378183917bebb1d17c94
it2022130 <it2022113@hua.gr>
2025-01-21 18:51:31 +0200

    Fronted Commit 4

--- FrontEnd/src/views/HomeView.vue
+++ FrontEnd/src/views/HomeView.vue
@@ -4,30 +4,30 @@ const { userData } = useApplicationStore();
 </script>
 
 <template>
-    <div class="bg-body-tertiary">
-        <div class="container">
-            <div class="row py-4 px-3">
-                <div class="col-12">
-                    <div class="mb-4">
-                        <h1 class="fs-3">Home</h1>
-                    </div>
-                    <div>
-                        <p>
-                            Logged in as: <strong>{{ userData.username }}</strong>
-                        </p>
-                        <ul>
-                            <!-- TODO enable.
-                            <li>
-                                <RouterLink :to="{ name: 'students' }">Manage Students</RouterLink>
-                            </li>
-                            -->
-                            <li>
-                                <RouterLink :to="{ name: 'courses' }">Manage Courses</RouterLink>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
+  <div class="bg-body-tertiary">
+    <div class="container">
+      <div class="row py-4 px-3">
+        <div class="col-12">
+          <div class="mb-4">
+            <h1 class="fs-3">Home</h1>
+          </div>
+          <div>
+            <p>
+              Logged in as: <strong>{{ userData.username }}</strong>
+            </p>
+            <ul>
+              <!-- TODO enable.
+              <li>
+                  <RouterLink :to="{ name: 'users' }">Manage Users</RouterLink>
+              </li>
+              -->
+              <li>
+                <RouterLink :to="{ name: 'properties' }">Manage Properties</RouterLink>
+              </li>
+            </ul>
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 </template>
