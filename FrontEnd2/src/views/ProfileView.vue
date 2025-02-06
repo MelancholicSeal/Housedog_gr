a835c767d49086b739c6f9e91b25042ebebd70a8
it2022115 <it2022115@hua.gr>
2025-02-06 19:38:56 +0200

    Profile View

--- FrontEnd2/src/views/ProfileView.vue
+++ FrontEnd2/src/views/ProfileView.vue
@@ -1,6 +1,20 @@
 <script setup>
 import { useApplicationStore } from '@/stores/application.js';
+import {onMounted, ref} from "vue";
+import {useRemoteData} from "@/composables/useRemoteData.js";
 const { userData } = useApplicationStore();
+
+const backendEnvVar = import.meta.env.VITE_BACKEND;
+
+const urlRef = ref(backendEnvVar + '/api/users/' + userData.id);
+const authRef = ref(true);
+const data = ref(null);
+const {loading, performRequest: getUserData} = useRemoteData(urlRef, authRef,data);
+
+onMounted(() => {
+  getUserData();
+});
+
 </script>
 
 <template>
@@ -15,14 +29,19 @@ const { userData } = useApplicationStore();
                         <!-- @EXERCISE: Create a nice component to present user data -->
                         
                 <div class="card" style="width: 18rem;">
-                <div class="card-body">
-                    <h5 class="card-title">{{ userData.username }}</h5>
+                  <div class="card-body">
+                    <h5 class="card-title">{{ data.username }}</h5>
                     <h6 class="card-subtitle mb-2 text-muted">Details</h6>
-                    <p class="card-text"> id: {{ userData.id }}</p>
-                    <p class="card-text"> roles: {{ userData.roles }}</p>
+                    <p class="card-text"> First Name: {{ data.firstName }}</p>
+                    <p class="card-text"> Last Name: {{ data.lastName }}</p>
+                    <p class="card-text"> Email: {{ data.email }}</p>
+                    <p class="card-text"> Phone Number: {{ data.phoneNumber }}</p>
+                    <p v-if="data.role === 'ROLE_USER'" class="card-text"> Role: User</p>
+                    <p v-else-if="data.role === 'ROLE_OWNER'" class="card-text"> Role: Owner</p>
+                    <p v-else class="card-text"> Role: Admin</p>
+                  </div>
                 </div>
-                </div>
-                <pre>{{ userData }}</pre>
+                <!--<pre>{{ userData }}</pre>-->
                     </div>
                 </div>
             </div>
