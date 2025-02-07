e967397eac6c293015aacf903c5edee71bda8a60
it2022115 <it2022115@hua.gr>
2025-02-07 18:27:16 +0200

    Profile View Update 3.0

--- FrontEnd2/src/views/ProfileView.vue
+++ FrontEnd2/src/views/ProfileView.vue
@@ -41,9 +41,7 @@ onMounted(() => {
                     <p v-else-if="userData?.roles?.includes('ROLE_OWNER')" class="card-text"> Role: Owner</p>
                     <p v-else class="card-text"> Role: Admin</p>
                     <p v-if="userData?.roles?.includes('ROLE_OWNER')" class="card-text">
-                      <li>
                         <RouterLink :to="{ name: 'user-properties' }"> My Properties</RouterLink>
-                      </li>
                     </p>
                   </div>
                 </div>
